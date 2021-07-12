8b6e98194fcc09eec12dea16eba88290bcf163a5
Iuly2000 <73794742+Iuly2000@users.noreply.github.com>
2021-07-12 16:43:50 +0300

    start login

--- CarDealership/CarDealership/MVVM/ViewModel/LoginWindowVM.cs
+++ CarDealership/CarDealership/MVVM/ViewModel/LoginWindowVM.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using CarDealership.Helpers;
+using CarDealership.MVVM.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace CarDealership.MVVM.ViewModel
 {
     class LoginWindowVM
     {
+        private ICommand loginCommand;
+        public ICommand LoginCommand
+        {
+            get
+            {
+                if (loginCommand == null)
+                {
+                    loginCommand = new RelayCommand<LoginWindow>(Logging);
+                }
+                return loginCommand;
+            }
+        }
+        private void Logging(LoginWindow window)
+        {
+            string email = window.txtemail.Text;
+            string password = window.txtpassword.Password;
+
+            if (email == "Admin" && password == "123")
+            {
+                window.Hide();
+                AdminWindow admin = new AdminWindow();
+                admin.ShowDialog();
+            }
+            else MessageBox.Show("Email si/sau parola gresite!", "Avertizare", MessageBoxButton.OK, MessageBoxImage.Warning);
+            
+        }
+
     }
 }
